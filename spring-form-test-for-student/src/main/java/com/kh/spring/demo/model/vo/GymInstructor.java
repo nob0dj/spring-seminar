c84cfaf802f80ce97d46eeb23ef898a7390625dd
shqkel <shqkel1863@gmail.com>
2019-09-10 10:48:53 +0900

    spring-form-test-for-student project init!

---
+++ spring-form-test-for-student/src/main/java/com/kh/spring/demo/model/vo/GymInstructor.java
@@ -0,0 +1,11 @@
+package com.kh.spring.demo.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class GymInstructor {
+	private String code;
+	private String name;
+}
